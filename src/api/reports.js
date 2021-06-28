22e76b94796cccc686a21d4338d6378b861d5323
Mauricio Boxtael <mboxtael@gmail.com>
2021-06-27 21:50:38 -0400

    new regulations

--- src/api/reports.js
+++ src/api/reports.js
@@ -1,17 +1,23 @@
 import { get } from '../util/request';
 
 export const listTickets = async (date) => {
-    const res = await get(`/v1/reports/consult_tickets`, {
+    const res = await get('/v1/reports/consult_tickets', {
         params: { date_from: date },
     });
 
     return res.data;
 };
 
-export const listAwards = async (date, type) => {
-    const res = await get(`/v1/reports/awards`, {
-        params: { date, tipo: Number(type) },
+export const listAwards = async (date) => {
+    const res = await get('/v1/reports/lotery_results', {
+        params: { date_from: date },
     });
+
+    return res.data;
+};
+
+export const listRegulations = async () => {
+    const res = await get('/v1/reports/regulations');
 
     return res.data;
 };
